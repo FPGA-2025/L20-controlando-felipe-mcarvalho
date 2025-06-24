8a1bbf3c27dee72cb604eab337b16756894e67d6
Felipe Carvalho <felipemarinho92020@gmail.com>
2025-06-24 18:51:23 -0300

    Adicionando tarefa

--- fifo.v
+++ fifo.v
@@ -16,6 +16,33 @@ module fifo(
     output reg [3:0] fifo_words  // Current number of elements
 );
 
-//insira seu código aqui
 
+    reg [7:0] mem [0:7];
+    reg [2:0] rd_ptr, wr_ptr;
+
+    wire do_wr = wr_en & ~full;
+    wire do_rd = rd_en & ~empty;
+
+    assign full  = (fifo_words == 4'd8);
+    assign empty = (fifo_words == 4'd0);
+
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            rd_ptr     <= 3'd0;
+            wr_ptr     <= 3'd0;
+            fifo_words <= 4'd0;
+            data_out   <= 8'd0;
+        end 
+        else begin
+            if (do_wr) begin
+                mem[wr_ptr] <= data_in;
+                wr_ptr      <= wr_ptr + 3'd1;
+            end
+            if (do_rd) begin
+                data_out <= mem[rd_ptr];
+                rd_ptr   <= rd_ptr + 3'd1;
+            end
+            fifo_words <= fifo_words + do_wr - do_rd;
+        end
+    end
 endmodule
--- fsm.v
+++ fsm.v
@@ -9,6 +9,39 @@ module fsm(
     input [3:0] fifo_words
 );
 
-// insira seu código aqui
 
+    parameter WRITE = 1'b0, 
+              STOP = 1'b1;
+    reg state;
+
+
+    assign fifo_data = 8'hAA;
+
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            state <= WRITE;
+            wr_en <= 1'b1;
+        end 
+        else begin
+            case (state)
+                WRITE: begin
+                    if (fifo_words >= 4'd5) begin
+                        wr_en <= 1'b0;
+                        state <= STOP;
+                    end else begin
+                        wr_en <= 1'b1;
+                    end
+                end
+                STOP: begin
+                    if (fifo_words <= 4'd2) begin
+                        wr_en <= 1'b1;
+                        state <= WRITE;
+                    end 
+                    else begin
+                        wr_en <= 1'b0;
+                    end
+                end
+            endcase
+        end
+    end
 endmodule
